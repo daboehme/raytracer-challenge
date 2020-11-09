e5145f79bb50b919e99cc073054dc86b33db9145
David Boehme <dboehme42@gmail.com>
2020-11-09 00:25:38 +0000

    Fix red color

--- src/canvas.rs
+++ src/canvas.rs
@@ -15,7 +15,7 @@ impl Color {
     }
 
     pub const BLACK: Color = Color { r: 0.0, g: 0.0, b: 0.0 };
-    pub const RED:   Color = Color { r: 0.0, g: 0.0, b: 0.0 };
+    pub const RED:   Color = Color { r: 1.0, g: 0.0, b: 0.0 };
 }
 
 pub struct Canvas {
